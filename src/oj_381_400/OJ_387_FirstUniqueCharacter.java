6f04f7d5b4311a87c241e1c24b9386964e169880
Rohan Gadad <rohan.gadad@gmail.com>
2016-08-22 13:57:01 -0400

    387

---
+++ src/oj_381_400/OJ_387_FirstUniqueCharacter.java
@@ -0,0 +1,30 @@
+package oj_381_400;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class OJ_387_FirstUniqueCharacter {
+	public int firstUniqChar(String s) {
+        int ans = -1;
+        if (s != null) {
+            Map<Character,Boolean> map = new LinkedHashMap<>();
+            for (int i=0; i<s.length(); i++) {
+                if (map.containsKey(s.charAt(i))) {
+                    map.put(s.charAt(i), false);
+                } else {
+                    map.put(s.charAt(i), true);
+                }
+            }
+            for (Map.Entry<Character,Boolean> entry : map.entrySet()) {
+                if (entry.getValue() == true) {
+                    for (int i=0; i<s.length(); i++) {
+                        if (s.charAt(i) == entry.getKey()) {
+                            return i;
+                        }
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
