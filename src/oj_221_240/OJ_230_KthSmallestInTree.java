3df11a6589d51434f67c06e95b4e7ce694cc4f8c
Rohan Gadad <rohan.gadad@gmail.com>
2016-08-24 23:04:46 -0400

    230

---
+++ src/oj_221_240/OJ_230_KthSmallestInTree.java
@@ -0,0 +1,25 @@
+package oj_221_240;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import common.TreeNode;
+
+public class OJ_230_KthSmallestInTree {
+	List<Integer> list = new ArrayList<>();
+
+	public int kthSmallest(TreeNode root, int k) {
+		inorder(root, k);
+		return list.get(k - 1);
+	}
+
+	private void inorder(TreeNode root, int k) {
+		if (list.size() != k) {
+			if (root != null) {
+				inorder(root.left, k);
+				list.add(root.val);
+				inorder(root.right, k);
+			}
+		}
+	}
+}
