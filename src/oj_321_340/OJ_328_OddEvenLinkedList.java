b5e97e2a14c50cb8f5c6f81a4a8d87180febb111
Rohan Gadad <rohan.gadad@gmail.com>
2016-08-24 22:05:31 -0400

    328

---
+++ src/oj_321_340/OJ_328_OddEvenLinkedList.java
@@ -0,0 +1,27 @@
+package oj_321_340;
+
+import common.ListNode;
+
+public class OJ_328_OddEvenLinkedList {
+	public ListNode oddEvenList(ListNode head) {
+		if (head == null || head.next == null || head.next.next == null) {
+			return head;
+		}
+		
+        ListNode ans = new ListNode(head.val), cur = head;
+        ListNode x = ans;
+        for (int i=0;i<2; i++) {
+            while (cur != null && cur.next != null && cur.next.next != null) {
+                ans.next = new ListNode(cur.next.next.val);
+                ans = ans.next;
+                cur = cur.next.next;
+            }
+            if (i==0) {
+                cur = head.next;
+                ans.next = new ListNode(cur.val);
+                ans = ans.next;
+            }
+        }
+        return x;
+    }
+}
