8fb165e84bf193055bab185fbe3ddbcbfad25d9f
Rohan Gadad <rohan.gadad@gmail.com>
2016-08-26 23:32:24 -0400

    70

---
+++ src/oj_061_080/OJ_070_ClimbingStairs.java
@@ -0,0 +1,20 @@
+package oj_061_080;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OJ_070_ClimbingStairs {
+	List<Integer> list = new ArrayList<>();
+
+	public OJ_070_ClimbingStairs() {
+        list.add(1);
+        list.add(1);
+    }
+
+	public int climbStairs(int n) {
+		for (int i = 2; i <= n; i++) {
+			list.add(list.get(i - 1) + list.get(i - 2));
+		}
+		return list.get(n);
+	}
+}
