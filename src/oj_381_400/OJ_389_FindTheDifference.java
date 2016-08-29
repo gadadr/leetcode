18d58cd2f00e11dafdd8cba45093336f88948490
Rohan Gadad <rohan.gadad@gmail.com>
2016-08-29 10:03:07 -0400

    389

---
+++ src/oj_381_400/OJ_389_FindTheDifference.java
@@ -0,0 +1,14 @@
+package oj_381_400;
+
+public class OJ_389_FindTheDifference {
+	public char findTheDifference(String s, String t) {
+        int ans = 0;
+        for (int i=0; i<s.length(); i++) {
+            ans ^= s.charAt(i);
+        }
+        for (int i=0; i<t.length(); i++) {
+            ans ^= t.charAt(i);
+        }
+        return (char) ans;
+    }
+}
