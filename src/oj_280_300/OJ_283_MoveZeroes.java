1c05acb97189148350aa8c3bf0ff85da47c8d555
Rohan Gadad <rohan.gadad@gmail.com>
2016-09-07 19:02:25 -0400

    Improved solution. From 6 to 19 percentile.

--- src/oj_280_300/OJ_283_MoveZeroes.java
+++ src/oj_280_300/OJ_283_MoveZeroes.java
@@ -2,14 +2,14 @@ package oj_280_300;
 
 public class OJ_283_MoveZeroes {
 	public void moveZeroes(int[] nums) {
+		int lastZero = nums.length-1;
         for (int i=nums.length-1; i>=0; i--) {
-        	if (nums[i] == 0) {
-        		int j = i;
-        		while (j<nums.length-1 && nums[j] == 0) {
-        			nums[j] = nums[j+1];
-        			nums[j+1] = 0;
-        			j++;
+        	if (nums[i] == 0 && i < lastZero) {
+        		for (int j=i; j<lastZero; j++) {
+        		    nums[j] = nums[j+1];
         		}
+        		nums[lastZero] = 0;
+        		lastZero--;
         	}
         }
     }
