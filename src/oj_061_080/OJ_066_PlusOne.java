5871ea60d5ad5b8636349c211efcde92963ca917
Rohan Gadad <rohan.gadad@gmail.com>
2016-09-29 17:28:58 -0400

    66

---
+++ src/oj_061_080/OJ_066_PlusOne.java
@@ -0,0 +1,20 @@
+package oj_061_080;
+
+public class OJ_066_PlusOne {
+	public int[] plusOne(int[] digits) {
+        for (int i=digits.length-1; i>=0; i--) {
+            if (digits[i] == 9) {
+                digits[i] = 0;
+            } else {
+                digits[i]++;
+                return digits;
+            }
+        }
+        int[] ans = new int[digits.length+1];
+        ans[0] = 1;
+        for (int i=1; i<ans.length; i++) {
+            ans[i] = digits[i-1];
+        }
+        return ans;
+    }
+}
