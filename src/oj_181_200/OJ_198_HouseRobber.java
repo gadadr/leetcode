3e42cc161c6b885c09b6c4fb7d771ab6a2f73afa
Rohan Gadad <rohan.gadad@gmail.com>
2016-09-29 16:49:48 -0400

    198

---
+++ src/oj_181_200/OJ_198_HouseRobber.java
@@ -0,0 +1,28 @@
+package oj_181_200;
+
+public class OJ_198_HouseRobber {
+	public int rob(int[] nums) {
+        int ans = 0;
+        switch (nums.length) {
+        case 0 : break;
+		case 1: ans = nums[0];
+			break;
+		case 2: ans = Math.max(nums[0], nums[1]);
+			break;
+		default:
+			int[] arr = new int[nums.length];
+			arr[0] = nums[0];
+			arr[1] = Math.max(nums[0], nums[1]);
+			for (int i=2; i<nums.length; i++) {
+	        	if (arr[i-1] == arr[i-2]) {
+	        		arr[i] = arr[i-1] + nums[i];
+	        	} else {
+	        		arr[i] = Math.max(arr[i-1], arr[i-2] + nums[i]);
+	        	}
+	        }
+			ans = arr[arr.length-1];
+			break;
+		}
+        return ans;
+    }
+}
