4962d6b29c64448ae171898a6dbc4211c1997c40
Rohan Gadad <rohan.gadad@gmail.com>
2016-10-11 22:39:04 -0400

    125

--- src/oj_121_140/_136_SingleNumber.java
+++ src/oj_121_140/_136_SingleNumber.java
@@ -1,6 +1,6 @@
 package oj_121_140;
 
-public class OJ_136_SingleNumber {
+public class _136_SingleNumber {
 	public int singleNumber(int[] nums) {
 		int x = nums[0];
         for (int i=1; i< nums.length; i++) {
