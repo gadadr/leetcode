a7ccb45e0a78c277b2c5d371331cb71f9573ac65
Rohan Gadad <rohan.gadad@gmail.com>
2019-01-18 19:53:11 -0700

    TreeNode now accepts one or more integers and creates a tree out of it

--- src/main/java/model/TreeNode.java
+++ src/main/java/model/TreeNode.java
@@ -1,10 +1,36 @@
 package model;
 
+import java.util.LinkedList;
+
 public class TreeNode {
 
     public int val;
     public TreeNode left;
     public TreeNode right;
 
-    public TreeNode(int x) { val = x; }
+    public TreeNode(Integer ... x) {
+        if (x.length == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        this.val = x[0];
+        if (x.length > 1) {
+            LinkedList<TreeNode> list = new LinkedList<>();
+            list.add(this);
+            for (int i = 1; i < x.length; i+=2) {
+                if(list.isEmpty()) {
+                    throw new IllegalStateException();
+                }
+                TreeNode node = list.pop();
+                if (x[i] != null) {
+                    node.left = new TreeNode(x[i]);
+                    list.add(node.left);
+                }
+                if (i+1 < x.length && x[i+1] != null) {
+                    node.right = new TreeNode(x[i+1]);
+                    list.add(node.right);
+                }
+            }
+        }
+    }
 }
