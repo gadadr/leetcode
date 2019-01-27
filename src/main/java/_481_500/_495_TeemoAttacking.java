6faf1a0a9e8204f67a547c1aa966aad172a0deed
Rohan Gadad <rohan.gadad@gmail.com>
2019-01-26 22:45:00 -0700

    495	Teemo Attacking

---
+++ src/main/java/_481_500/_495_TeemoAttacking.java
@@ -0,0 +1,17 @@
+package _481_500;
+
+public class _495_TeemoAttacking {
+
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+        if (timeSeries.length == 0) {
+            return 0;
+        }
+        int ans = 0;
+        for (int i=0; i<timeSeries.length-1; i++) {
+            ans += Math.min(timeSeries[i+1]-timeSeries[i], duration);
+        }
+        ans += duration;
+        return ans;
+    }
+
+}
